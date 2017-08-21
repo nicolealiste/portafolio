4371ee6b74972f7d56ab45c8fa4ff04c19da3519
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-08-21 00:49:16 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	//menu responsive
+	 $(".button-collapse").sideNav();
+
+	//corrousel
+	$('.carousel').carousel();
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+});
